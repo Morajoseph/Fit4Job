07cd2f0a0f59ab2b2fa01c917fc2f8589f3a3af6
NourhanMMourise <magednourhan66@gmail.com>
2025-06-28 13:43:26 +0300

    Add Skill class with validation and status properties
    Introduces a new `Skill` class in the `Fit4Job.Models` namespace.
    The class includes properties for `Id`, `Name`, `Description`,
    `CreatedAt`, `DeletedAt`, and `IsActive`, with validation on the
    `Name` property to ensure it meets specified requirements.
    `CreatedAt` is set to the current date and time by default,
    while `DeletedAt` is nullable. The `IsActive` property defaults
    to true, indicating the skill is active.

---
+++ Fit4Job/Models/Skill.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Fit4Job.Models
+{
+    public class Skill
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Skill Name")]
+        [Required(ErrorMessage = "Skill  name is required.")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Skill Name must be between 2 and 50 characters.")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Created At")]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Deleted At")]
+        public DateTime? DeletedAt { get; set; }
+        public bool IsActive { get; set; } = true;
+
+
+    }
+}
