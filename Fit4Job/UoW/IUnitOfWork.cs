fdbd1e32e5f85289050351639a58d7e486ab44b2
YousryEssam <youssry.essam2111@gmail.com>
2025-07-05 07:34:40 +0300

    Refactor Unit of Work implementation and project file
    Updated `Fit4Job.csproj` to remove `UoW` directory references.
    Added `IUnitOfWork` interface and implemented `UnitOfWork` class
    to manage repository instances and handle database transactions
    with methods for completing and rolling back transactions.

---
+++ Fit4Job/UoW/IUnitOfWork.cs
@@ -0,0 +1,31 @@
+﻿namespace Fit4Job.UoW
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IAdminProfileRepository AdminProfileRepository { get; }
+        IApplicationUserRepository ApplicationUserRepository { get; }
+        IBadgeRepository BadgeRepository { get; }
+        ICompanyExamAttemptRepository CompanyExamAttemptRepository { get; }
+        ICompanyExamQuestionAnswerRepository CompanyExamQuestionAnswerRepository { get; }
+        ICompanyExamQuestionOptionRepository CompanyExamQuestionOptionRepository { get; }
+        ICompanyExamQuestionRepository CompanyExamQuestionRepository { get; }
+        ICompanyExamRepository CompanyExamRepository { get; }
+        ICompanyProfileRepository CompanyProfileRepository { get; }
+        ICompanyTaskRepository CompanyTaskRepository { get; }
+        ICompanyTaskSubmissionRepository CompanyTaskSubmissionRepository { get; }
+        IJobSeekerProfileRepository JobSeekerProfileRepository { get; }
+        INotificationRepository NotificationRepository { get; }
+        IPaymentRepository PaymentRepository { get; }
+        ISkillRepository SkillRepository { get; }
+        ITrackAttemptRepository TrackAttemptRepository { get; }
+        ITrackCategoryRepository TrackCategoryRepository { get; }
+        ITrackQuestionAnswerRepository TrackQuestionAnswerRepository { get; }
+        ITrackQuestionOptionRepository TrackQuestionOptionRepository { get; }
+        ITrackQuestionRepository TrackQuestionRepository { get; }
+        ITrackRepository TrackRepository { get; }
+        IUserBadgeRepository UserBadgeRepository { get; }
+        IUserSkillRepository UserSkillRepository { get; }
+        Task<int> CompleteAsync();
+        Task RollbackAsync();
+    }
+}
---
+++ Fit4Job/UoW/UnitOfWork.cs
@@ -0,0 +1,98 @@
+﻿using Fit4Job.Repositories.Implementations;
+
+namespace Fit4Job.UoW
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly Fit4JobDbContext _context;
+
+        public IAdminProfileRepository AdminProfileRepository { get; }
+
+        public IApplicationUserRepository ApplicationUserRepository { get; }
+
+        public IBadgeRepository BadgeRepository { get; }
+
+        public ICompanyExamAttemptRepository CompanyExamAttemptRepository { get; }
+
+        public ICompanyExamQuestionAnswerRepository CompanyExamQuestionAnswerRepository { get; }
+
+        public ICompanyExamQuestionOptionRepository CompanyExamQuestionOptionRepository { get; }
+
+        public ICompanyExamQuestionRepository CompanyExamQuestionRepository { get; }
+
+        public ICompanyExamRepository CompanyExamRepository { get; }
+
+        public ICompanyProfileRepository CompanyProfileRepository { get; }
+
+        public ICompanyTaskRepository CompanyTaskRepository { get; }
+
+        public ICompanyTaskSubmissionRepository CompanyTaskSubmissionRepository { get; }
+
+        public IJobSeekerProfileRepository JobSeekerProfileRepository { get; }
+
+        public INotificationRepository NotificationRepository { get; }
+
+        public IPaymentRepository PaymentRepository { get; }
+
+        public ISkillRepository SkillRepository { get; }
+
+        public ITrackAttemptRepository TrackAttemptRepository { get; }
+
+        public ITrackCategoryRepository TrackCategoryRepository { get; }
+
+        public ITrackQuestionAnswerRepository TrackQuestionAnswerRepository { get; }
+
+        public ITrackQuestionOptionRepository TrackQuestionOptionRepository { get; }
+
+        public ITrackQuestionRepository TrackQuestionRepository { get; }
+
+        public ITrackRepository TrackRepository { get; }
+
+        public IUserBadgeRepository UserBadgeRepository { get; }
+
+        public IUserSkillRepository UserSkillRepository { get; }
+
+        public UnitOfWork(Fit4JobDbContext context)
+        {
+            _context = context;
+            AdminProfileRepository = new AdminProfileRepository(_context);
+            ApplicationUserRepository = new ApplicationUserRepository(_context);
+            BadgeRepository = new BadgeRepository(_context);
+            CompanyExamAttemptRepository = new CompanyExamAttemptRepository(_context);
+            CompanyExamQuestionAnswerRepository = new CompanyExamQuestionAnswerRepository(_context);
+            CompanyExamQuestionOptionRepository = new CompanyExamQuestionOptionRepository(_context);
+            CompanyExamQuestionRepository = new CompanyExamQuestionRepository(_context);
+            CompanyExamRepository = new CompanyExamRepository(_context);
+            CompanyProfileRepository = new CompanyProfileRepository(_context);
+            CompanyTaskRepository = new CompanyTaskRepository(_context);
+            CompanyTaskSubmissionRepository = new CompanyTaskSubmissionRepository(_context);
+            JobSeekerProfileRepository = new JobSeekerProfileRepository(_context);
+            NotificationRepository = new NotificationRepository(_context);
+            PaymentRepository = new PaymentRepository(_context);
+            SkillRepository = new SkillRepository(_context);
+            TrackAttemptRepository = new TrackAttemptRepository(_context);
+            TrackCategoryRepository = new TrackCategoryRepository(_context);
+            TrackQuestionAnswerRepository = new TrackQuestionAnswerRepository(_context);
+            TrackQuestionOptionRepository = new TrackQuestionOptionRepository(_context);
+            TrackQuestionRepository = new TrackQuestionRepository(_context);
+            TrackRepository = new TrackRepository(_context);
+            UserBadgeRepository = new UserBadgeRepository(_context);
+            UserSkillRepository = new UserSkillRepository(_context);
+        }
+
+        public async Task<int> CompleteAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public async Task RollbackAsync()
+        {
+            await _context.Database.RollbackTransactionAsync();
+        }
+    }
+}
