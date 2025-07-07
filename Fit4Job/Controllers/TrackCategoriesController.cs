43637ec95f44dcb2ea77632a9435d79706055749
YousryEssam <youssry.essam2111@gmail.com>
2025-07-07 17:06:34 +0300

    Create TrackCategoriesController

---
+++ Fit4Job/Controllers/TrackCategoriesController.cs
@@ -0,0 +1,15 @@
+﻿namespace Fit4Job.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrackCategoriesController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public TrackCategoriesController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+
+    }
+}
