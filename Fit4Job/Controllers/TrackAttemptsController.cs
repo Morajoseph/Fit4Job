24c291967a78984570f3f834d318a2a93f6541f0
YousryEssam <youssry.essam2111@gmail.com>
2025-07-08 18:03:07 +0300

    Create TracksController & TrackAttemptsController

---
+++ Fit4Job/Controllers/TrackAttemptsController.cs
@@ -0,0 +1,16 @@
+﻿namespace Fit4Job.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrackAttemptsController : ControllerBase
+    {
+        private readonly IUnitOfWork unitOfWork;
+        public TrackAttemptsController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+
+
+    }
+}
---
+++ Fit4Job/Controllers/TracksController.cs
@@ -0,0 +1,15 @@
+﻿namespace Fit4Job.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TracksController : ControllerBase
+    {
+        private readonly IUnitOfWork unitOfWork;
+        public TracksController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+
+    }
+}
