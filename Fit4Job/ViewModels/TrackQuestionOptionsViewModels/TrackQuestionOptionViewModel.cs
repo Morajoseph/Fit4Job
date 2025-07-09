106d22e80068466a582291e74507d124d99a8885
Mora Joseph <morajoseph72@gmail.com>
2025-07-09 22:17:21 +0300

    Add TrackQuestionOptionViewModel

---
+++ Fit4Job/ViewModels/TrackQuestionOptionsViewModels/TrackQuestionOptionViewModel.cs
@@ -0,0 +1,34 @@
+﻿namespace Fit4Job.ViewModels.TrackQuestionOptionsViewModels
+{
+    public class TrackQuestionOptionViewModel
+    {
+
+
+        [Display(Name = "Option ID")]
+        public int Id { get; set; }
+
+        [Display(Name = "Question ID")]
+        public int QuestionId { get; set; }
+
+        [Display(Name = "Option Text")]
+        public string OptionText { get; set; } = string.Empty;
+
+        [Display(Name = "Is Correct")]
+        public bool IsCorrect { get; set; }
+
+        public TrackQuestionOptionViewModel() { }
+
+        public TrackQuestionOptionViewModel(TrackQuestionOption option)
+        {
+            Id = option.Id;
+            QuestionId = option.QuestionId;
+            OptionText = option.OptionText;
+            IsCorrect = option.IsCorrect;
+        }
+
+        public static TrackQuestionOptionViewModel GetViewModel(TrackQuestionOption option)
+        {
+            return new TrackQuestionOptionViewModel(option);
+        }
+    }
+}
