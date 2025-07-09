df3394d2a9a6a78455bce091cc96e85bd8ed9a58
NourhanMMourise <magednourhan66@gmail.com>
2025-07-09 22:29:22 +0300

    Add endpoints for managing track questions
    - Updated `TrackQuestionsController.cs` with new using directives.
    - Implemented soft delete functionality for track questions.
    - Added methods to retrieve questions by track ID and filter by type and level.
    - Enhanced error handling and response formatting for better usability.

--- Fit4Job/Controllers/TrackQuestionsController.cs
+++ Fit4Job/Controllers/TrackQuestionsController.cs
@@ -1,4 +1,7 @@
-﻿namespace Fit4Job.Controllers
+﻿using Fit4Job.Enums;
+using Fit4Job.ViewModels.TracksViewModels;
+
+namespace Fit4Job.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
@@ -14,6 +17,89 @@
             this.userManager = userManager;
         }
         /* ****************************************** Endpoints ****************************************** */
+
+        // track question soft delete
+
+        [HttpDelete("Delete/{id:int}")]
+
+        public async Task<ApiResponse<string>> SoftDeleteQuestion(int id)
+        {
+            var trackQuestion = await unitOfWork.TrackQuestionRepository.GetByIdAsync(id);
+            if (trackQuestion == null)
+            {
+                return ApiResponseHelper.Error<string>(ErrorCode.NotFound, "Question not found");
+            }
+            if (trackQuestion.DeletedAt != null)
+            {
+                return ApiResponseHelper.Error<string>(ErrorCode.BadRequest, "Question is already deleted");
+
+            }
+            trackQuestion.DeletedAt = DateTime.UtcNow;
+            trackQuestion.UpdatedAt = DateTime.UtcNow;
+
+
+            unitOfWork.TrackQuestionRepository.Update(trackQuestion);
+            await unitOfWork.CompleteAsync();
+
+            return ApiResponseHelper.Success(" Question deleted successfully ");
+
+        }
+
+        //=======================================================================
+
+
+        [HttpGet("track/{trackId:int}")]
+        public async Task<ApiResponse<IEnumerable<TrackQuestionViewModel>>>GetQuestionsByTrackId(int trackId)
+
+        {
+            var track = await unitOfWork.TrackRepository.GetByIdAsync(trackId);
+            if(track == null)
+            {
+                return ApiResponseHelper.Error<IEnumerable<TrackQuestionViewModel>>(ErrorCode.BadRequest, "Invalid trackId");
+
+            }
+
+            var Questions = await unitOfWork.TrackQuestionRepository.GetQuestionsByTrackIdAsync(trackId);
+
+
+            var data = Questions.Select(q => TrackQuestionViewModel.GetViewModel(q));
+            return ApiResponseHelper.Success(data);
+
+
+        }
+
+
+        //=============================================================
+
+
+        [HttpGet("filter")]
+        public async Task<ApiResponse<IEnumerable<TrackQuestionViewModel>>> GetQuestionByTypeAndLevel(int trackId, QuestionType questionType, QuestionLevel questionLevel)
+        {
+            if (trackId <= 0)
+            {
+                return ApiResponseHelper.Error<IEnumerable<TrackQuestionViewModel>>(
+                    ErrorCode.BadRequest,
+                    "Invalid trackId"
+                );
+            }
+
+            var track = await unitOfWork.TrackRepository.GetByIdAsync(trackId);
+            if (track == null)
+            {
+                return ApiResponseHelper.Error<IEnumerable<TrackQuestionViewModel>>( ErrorCode.NotFound,"Track not found");
+            }
+
+            var questions = await unitOfWork.TrackQuestionRepository.GetQuestionsByTrackIdAsync(trackId);
+
+            var filteredQuestions = questions.Where(q =>q.DeletedAt == null && q.QuestionType == questionType &&  q.QuestionLevel == questionLevel);
+
+           
+            var data = filteredQuestions.Select(q => TrackQuestionViewModel.GetViewModel(q));
+
+            return ApiResponseHelper.Success(data);
+        
+
+    }
 
 
 
