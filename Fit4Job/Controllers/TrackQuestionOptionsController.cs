37e334374cdca06b4a8b21d320a8989afbd62d42
YousryEssam <youssry.essam2111@gmail.com>
2025-07-09 20:21:11 +0300

    ADD TrackQuestionOptionsController , TrackQuestionsController

---
+++ Fit4Job/Controllers/TrackQuestionOptionsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fit4Job.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrackQuestionOptionsController : ControllerBase
+    {
+        /* ********************************************* DI ********************************************** */
+        private readonly IUnitOfWork unitOfWork;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public TrackQuestionOptionsController(IUnitOfWork unitOfWork, UserManager<ApplicationUser> userManager)
+        {
+            this.unitOfWork = unitOfWork;
+            this.userManager = userManager;
+        }
+        /* ****************************************** Endpoints ****************************************** */
+
+    }
+}
---
+++ Fit4Job/Controllers/TrackQuestionsController.cs
@@ -0,0 +1,21 @@
+﻿namespace Fit4Job.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrackQuestionsController : ControllerBase
+    {
+        /* ********************************************* DI ********************************************** */
+        private readonly IUnitOfWork unitOfWork;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public TrackQuestionsController(IUnitOfWork unitOfWork, UserManager<ApplicationUser> userManager)
+        {
+            this.unitOfWork = unitOfWork;
+            this.userManager = userManager;
+        }
+        /* ****************************************** Endpoints ****************************************** */
+
+
+
+    }
+}
