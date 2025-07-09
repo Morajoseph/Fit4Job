955a66f087ed5c4dc7a65fa6ee5e136bd1267f2c
Mora Joseph <morajoseph72@gmail.com>
2025-07-09 21:43:26 +0300

    Add endpoints of TrackQuestionController

--- Fit4Job/Controllers/TrackQuestionsController.cs
+++ Fit4Job/Controllers/TrackQuestionsController.cs
@@ -1,4 +1,6 @@
-﻿namespace Fit4Job.Controllers
+﻿using Fit4Job.ViewModels.TracksViewModels;
+
+namespace Fit4Job.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
@@ -15,6 +17,31 @@
         }
         /* ****************************************** Endpoints ****************************************** */
 
+        //Get all track questions.
+
+        [HttpGet]
+        public async Task<ApiResponse<IEnumerable<TrackQuestionViewModel>>> GetAllTrackQuestions()
+        {
+            var questions = await unitOfWork.TrackQuestionRepository.GetAllAsync();
+            var data = questions.Select(TrackQuestionViewModel.GetViewModel);
+            return ApiResponseHelper.Success(data);
+        }
+
+
+        //Get a question by ID.
+
+        [HttpGet("{id:int}")]
+        public async Task<ApiResponse<TrackQuestionViewModel>> GetById(int id)
+        {
+            var question = await unitOfWork.TrackQuestionRepository.GetByIdAsync(id);
+
+            if (question == null)
+                return ApiResponseHelper.Error<TrackQuestionViewModel>(ErrorCode.NotFound, "Question not found");
+
+            var viewModel = TrackQuestionViewModel.GetViewModel(question);
+
+            return ApiResponseHelper.Success(viewModel);
+        }
 
 
     }
