356e97c6428a6f730d8ab7ca90f0da76e08e615d
YousryEssam <youssry.essam2111@gmail.com>
2025-07-11 04:32:26 +0300

    Add TrackQuestionAnswersController

---
+++ Fit4Job/Controllers/TrackQuestionAnswersController.cs
@@ -0,0 +1,20 @@
+﻿namespace Fit4Job.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrackQuestionAnswersController : ControllerBase
+    {
+        /* ********************************************* DI ********************************************** */
+        private readonly IUnitOfWork unitOfWork;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public TrackQuestionAnswersController(IUnitOfWork unitOfWork, UserManager<ApplicationUser> userManager)
+        {
+            this.unitOfWork = unitOfWork;
+            this.userManager = userManager;
+        }
+
+        /* ****************************************** Endpoints ****************************************** */
+
+    }
+}
