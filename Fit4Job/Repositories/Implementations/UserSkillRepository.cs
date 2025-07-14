57de26f178c8f1079899920d359f73b67d16b9b0
Mora Joseph <morajoseph72@gmail.com>
2025-07-14 21:39:41 +0300

    Add implementain for endpoint in UserSkillController

--- Fit4Job/Repositories/Implementations/UserSkillRepository.cs
+++ Fit4Job/Repositories/Implementations/UserSkillRepository.cs
@@ -14,5 +14,34 @@
                 .Where(us => us.UserId == userId && us.DeletedAt == null)
                 .ToListAsync();
         }
+
+        public async Task<UserSkill?> GetActiveUserSkillAsync(int userId, int skillId)
+        {
+            return await _context.UserSkills
+                .FirstOrDefaultAsync(us =>
+                    us.UserId == userId &&
+                    us.SkillId == skillId &&
+                    us.DeletedAt == null);
+        }
+
+        public async Task<IEnumerable<Skill>> GetActiveSkillsByUserIdAsync(int userId)
+        {
+            return await _context.UserSkills
+                .Where(us => us.UserId == userId && us.DeletedAt == null)
+                .Include(us => us.Skill)
+                .Select(us => us.Skill)
+                .ToListAsync();
+        }
+
+        public async Task<UserSkill?> GetDeletedUserSkillAsync(int userId, int skillId)
+        {
+            return await _context.UserSkills
+                .FirstOrDefaultAsync(us =>
+                    us.UserId == userId &&
+                    us.SkillId == skillId &&
+                    us.DeletedAt != null);
+        }
+
+
     }
 }
--- Fit4Job/Repositories/Interfaces/IUserSkillRepository.cs
+++ Fit4Job/Repositories/Interfaces/IUserSkillRepository.cs
@@ -3,6 +3,15 @@
     public interface IUserSkillRepository : IGenericRepository<UserSkill>
     {
         Task<IEnumerable<UserSkill>> GetSkillsByUserIdAsync(int userId);
+
+        Task<UserSkill?> GetActiveUserSkillAsync(int userId, int skillId);
+
+        Task<IEnumerable<Skill>> GetActiveSkillsByUserIdAsync(int userId);
+
+        Task<UserSkill?> GetDeletedUserSkillAsync(int userId, int skillId);
+
+
+
         //Task<bool> HasAnySkillFromListAsync(int userId, List<int> skillIds);
 
     }
