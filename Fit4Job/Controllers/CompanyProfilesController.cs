50d936a8ed2fc878af0a6b4e4d362c64c6b78fa7
NourhanMMourise <magednourhan66@gmail.com>
2025-07-16 11:35:16 +0300

    Refactor CompanyProfilesController and add endpoints
    - Moved `namespace` declaration and added necessary `using` directives.
    - Renamed private fields to follow naming conventions.
    - Updated constructor to use renamed fields.
    - Added `GetAllCompanyProfiles` and `GetById` endpoints with error handling for profile retrieval.

--- Fit4Job/Controllers/CompanyProfilesController.cs
+++ Fit4Job/Controllers/CompanyProfilesController.cs
@@ -1,19 +1,50 @@
-﻿namespace Fit4Job.Controllers
+﻿using Fit4Job.ViewModels.CompanyProfileViewModels;
+using Fit4Job.ViewModels.JobsViewModels;
+using Microsoft.AspNetCore.Identity;
+
+namespace Fit4Job.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class CompanyProfilesController : ControllerBase
     {
-        private readonly IUnitOfWork unitOfWork;
-        private readonly UserManager<ApplicationUser> userManager;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly UserManager<ApplicationUser> _userManager;
 
         public CompanyProfilesController(IUnitOfWork unitOfWork, UserManager<ApplicationUser> userManager)
         {
-            this.unitOfWork = unitOfWork;
-            this.userManager = userManager;
+            _unitOfWork = unitOfWork;
+            _userManager = userManager;
         }
 
         /* ****************************************** Endpoints ****************************************** */
+
+
+        [HttpGet]
+        public async Task<ApiResponse<IEnumerable<CompanyProfileViewModel>>> GetAllCompanyProfiles()
+        {
+
+            var companyProfiles = await unitOfWork.CompanyProfileRepository.GetAllAsync();
+            var data = companyProfiles.Select(cp => CompanyProfileViewModel.GetViewModel(cp));
+            return ApiResponseHelper.Success(data);
+
+        }
+
+
+
+        [HttpGet("{id:int}")]
+        public async Task<ApiResponse<CompanyProfileViewModel>> GetById(int id)
+        {
+            var companyProfile = await _unitOfWork.CompanyProfileRepository.GetByIdAsync(id);
+            if (companyProfile == null)
+            {
+                return ApiResponseHelper.Error<CompanyProfileViewModel>(ErrorCode.NotFound, "Not Found or invalid ID");
+            }
+            return ApiResponseHelper.Success(new CompanyProfileViewModel(companyProfile));
+        }
+
+
+
 
 
 
